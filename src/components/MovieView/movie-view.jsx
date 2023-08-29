2b1e150adc0eda39eb7fa26a6d8e0f89960f1e65
AleToMe20 <78073236+AleToMe20@users.noreply.github.com>
2023-08-29 11:40:38 +0200

    Add PropTypes for MovieView Component
     Define PropTypes for the MovieView component to ensure proper prop validation and improve codebase integrity. PropTypes are added to specify the expected shape of the movie prop, including its image, title, description, genre, and director properties. Additionally, the onBackClick function prop is defined to ensure type safety. These changes enhance the reliability and maintainability of the codebase.

--- src/components/MovieView/movie-view.jsx
+++ src/components/MovieView/movie-view.jsx
@@ -1,27 +1,40 @@
+import PropTypes from "prop-types";
+
 export const MovieView = ({ movie, onBackClick }) => {
-    return (
+  return (
+    <div>
       <div>
-        <div>
-          <img src={movie.image} />
-        </div>
-        <div>
-          <span>Title: </span>
-          <span>{movie.title}</span>
-        </div>
-        <div>
-            <span>Description: </span>
-            <span>{movie.description}</span>
-            </div>
-        <div>
-            <span>Genre: </span>
-            <span>{movie.genre}</span>
-        </div>
-        <div>
-            <span>Director: </span>
-            <span>{movie.director}</span>
-        </div>
-            <button onClick={onBackClick}>Back</button>
-        </div>
-    );
+        <img src={movie.image} alt={movie.title} /> {/* Add alt attribute */}
+      </div>
+      <div>
+        <span>Title: </span>
+        <span>{movie.title}</span>
+      </div>
+      <div>
+        <span>Description: </span>
+        <span>{movie.description}</span>
+      </div>
+      <div>
+        <span>Genre: </span>
+        <span>{movie.genre}</span>
+      </div>
+      <div>
+        <span>Director: </span>
+        <span>{movie.director}</span>
+      </div>
+      <button onClick={onBackClick}>Back</button>
+    </div>
+  );
 };
-  
+
+// Define PropTypes for MovieView component
+MovieView.propTypes = {
+  movie: PropTypes.shape({
+    image: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+    director: PropTypes.string.isRequired,
+  }).isRequired,
+  onBackClick: PropTypes.func.isRequired,
+};
